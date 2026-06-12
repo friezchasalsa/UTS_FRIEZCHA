8190a77770d775c5a22bd06bec7fe91c98a50ba6
friezchasalsa <friezchasalsa@gmail.com>
2026-06-12 19:06:41 +0700

    update code

--- friezcha/friezcha/settings.py
+++ friezcha/friezcha/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ix&!2u&dk9rh7%vbg6s2&non#v-3z%m2+n=f$+4(4o%sox4oa7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
